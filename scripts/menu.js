c7868fb5d9c4c1d071f53a1231e3336b924a98ef
Noel Guillen <noelblas274@gmail.com>
2024-01-27 22:45:38 -0500

    added URL PARM for object notation

---
+++ scripts/menu.js
@@ -0,0 +1,29 @@
+import data from './data.js';
+
+const heroImage = document.getElementById("hero-example");
+const heroText = document.querySelector(".hero-text");
+
+
+//*Make a function that takes one param.
+//* window.location.search: This gets the query string part of the URL ?q=breads).
+//* new URLSearchParams(queryString): This creates a new URLSearchParams object, which allows you to easily work with query parameters.
+//*urlParams.get(key): This retrieves the value associated with the specified key from the query parameters
+//* if you call getQueryParam('q'), it will return the value of the 'q' parameter in the current URL.
+
+function getQueryParam(key) {
+      const queryString = window.location.search;
+      const urlParams = new URLSearchParams(queryString);
+      return urlParams.get(key);
+    }
+
+    // Example usage
+    const menuType = getQueryParam('q');
+    if (menuType) {
+      console.log('Menu type:', menuType);
+
+    }
+
+    console.log(data)
+
+    heroImage.src = data[menuType].headerImage
+    heroText.innerText = menuType;
