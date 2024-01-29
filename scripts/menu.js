0f449600cb1c853a4838e3d2fd60066c8c574f25
Noel Guillen <noelblas274@gmail.com>
2024-01-28 21:34:42 -0500

    tina dom for hero section

--- scripts/menu.js
+++ scripts/menu.js
@@ -22,9 +22,16 @@ function getQueryParam(key) {
 
     }
 
-    console.log(data[menuType])
+    console.log(data[menuType].headerImage)
 
 // Use data[menuType] instead of data.menuType
 // Dot notation expects a constant property name,
 // while square bracket notation allows dynamic keys.
+
+const menuHeader = document.querySelector('.menu-page-hero');
+const menuHeaderText = document.querySelector('.menu-page-heading');
+
+menuHeader.setAttribute( "style", `background-image: url(${data[menuType].headerImage})` );
+menuHeaderText.innerText = data[menuType].items[0].category;
+
 
