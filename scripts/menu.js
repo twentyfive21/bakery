3eac6f3c1d1f2fa19898e2e54e3145051b13da7c
Noel Guillen <noelblas274@gmail.com>
2024-01-28 21:11:02 -0500

    added comments to search url parm

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,11 +1,13 @@
 import data from './data.js';
 
-
 //*Make a function that takes one param.
 //* window.location.search: This gets the query string part of the URL ?q=breads).
 //* new URLSearchParams(queryString): This creates a new URLSearchParams object, which allows you to easily work with query parameters.
 //*urlParams.get(key): This retrieves the value associated with the specified key from the query parameters
 //* if you call getQueryParam('q'), it will return the value of the 'q' parameter in the current URL.
+
+
+
 
 function getQueryParam(key) {
       const queryString = window.location.search;
@@ -20,4 +22,9 @@ function getQueryParam(key) {
 
     }
 
-    console.log(data)
+    console.log(data[menuType])
+
+// Use data[menuType] instead of data.menuType
+// Dot notation expects a constant property name,
+// while square bracket notation allows dynamic keys.
+
